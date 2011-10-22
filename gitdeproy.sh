b5c530dfa3f2bb14a961fceab97d1bc298a2054f
hidesys <git@hidesys.net>
2011-10-22 18:33:36 +0900

    雑誌掲載行為

---
+++ gitdeproy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+if 'pwd' = "/var/www/dev/mantropy/" -a $1
+then
+  git add .
+  git commit -m $1
+  git pull
+  git push
+  cd /var/www/mantropy/
+  git checkout go_on
+  git merge master
+  cd /var/www/dev/mantropy/
+fi
