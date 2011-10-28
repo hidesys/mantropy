764c0bbf511f4f470084c82292091f8d604e44f9
hidesys <git@hidesys.net>
2011-10-28 13:41:30 +0900

    kuso

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -10,8 +10,8 @@ class SeriesController < ApplicationController
     end
 
     unless params[:str] == "name" then
-      ranking_id = (Ranking.find_by_name(params[:str]) ? Ranking.find_by_name(params[:str]).id : 1)
-      unless ranking_id = 2 then
+      ranking_id = (Ranking.find_by_name(params[:str]) ? Ranking.find_by_name(params[:str]).id : 2)
+      unless ranking_id == 2 then
         sql = "SELECT s.id, s.name, \"純得点: \"||(rs.mark - (rs.count -1) * 3)||\"　補正後得点: \"||rs.mark||\"　重複数: \"||rs.count AS url FROM series s INNER JOIN (SELECT (SUM(31 - rank) + ((COUNT(*) - 1) * 3)) AS mark, serie_id, count(id) AS count from ranks where ranking_id=#{ranking_id} and rank between 1 and 30 group by serie_id) rs ON s.id=rs.serie_id order by rs.mark DESC, rs.count DESC"
       else
         sql = "SELECT s.id, s.name, \"純得点: \"||rs.mark||\"　補正後得点: \"||(rs.mark + COALESCE(rk.mark,0))||\"　重複数: \"||(COALESCE(rk.count,0)) AS url, (rs.mark + COALESCE(rk.mark,0)) AS amark FROM series s INNER JOIN (SELECT (SUM(31 - rank) + ((COUNT(*) - 1) * 3)) AS mark, serie_id, count(id) AS count from ranks where ranking_id=1 and rank between 1 and 30 group by serie_id) rs ON s.id=rs.serie_id LEFT JOIN (SELECT (SUM(rank - 6) + (COUNT(*) -1) * (-3)) AS mark, serie_id, count(id) AS count FROM ranks WHERE ranking_id=2 GROUP BY serie_id) rk ON s.id = rk.serie_id order by amark DESC, rk.count DESC"
