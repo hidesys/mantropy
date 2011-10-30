1f18cc5339853d596d5533e2c174177ae8f4199d
hidesys <git@hidesys.net>
2011-10-31 02:17:27 +0900

    us

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 class UsersController < ApplicationController
-  before_filter :authenticate_user!, :except => [:show, :index]
+  before_filter :authenticate_user!, :except => [:index]
   # GET /users.xml
   def index
     @title = "メンバー一覧"
