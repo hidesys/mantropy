f178403330aedf4625608ff5fdb75aa1d0f20ddf
hidesys <git@hidesys.net>
2012-12-08 19:45:55 +0900

    serie_show

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -220,7 +220,8 @@ class SeriesController < ApplicationController
     @serie = Serie.find(params[:id])
     @title = "#{@serie.name} のシリーズ情報"
     @similar_series = Serie.find_by_sql("SELECT s.* FROM series s INNER JOIN (SELECT r1.serie_id, COUNT(*) AS similarity FROM ranks r1 INNER JOIN ranks r2 ON r1.user_id=r2.user_id WHERE r2.serie_id=#{@serie.id} GROUP BY r1.serie_id ORDER BY similarity DESC, SUM(r1.score) DESC) r ON r.serie_id=s.id WHERE s.id!=#{@serie.id} LIMIT 4")
-    @ranks = @serie.ranks.where(:ranking_id => [1, 2, 3, 4]).order("ranking_id DESC, rank")
+    #@ranks = @serie.ranks.where(:ranking_id => [1, 2, 3, 4]).order("ranking_id DESC, rank")
+    @ranks = @serie.ranks.order("ranking_id DESC, rank")
 
     unless @serie.topic then
       topic = Topic.new
