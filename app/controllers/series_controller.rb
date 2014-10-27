128ef6bd8317fb396d32ccf8a7015cd8aaa2fc24
hidesys <info@hidesys.net>
2014-10-28 02:10:08 +0900

    ランキング集計をRankingから引っ張ってくるようにした

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -30,32 +30,36 @@ class SeriesController < ApplicationController
     #render "ranking_name"
     #return
 
-    ranking = Ranking.find_by_name(params[:str]) || Ranking.find(params[:str])
-    ranking_id = ranking ? ranking.id : 5
-    is_ranking_now = [1, 2, 5, 6, 7, 8].include? ranking_id
-    if is_ranking_now
-      ranking_id_plus = {1 => 1, 2 => 1, 5 => 5, 6 => 5, 7 => 7, 8 => 7}[ranking_id]
-      ranking_id_minus = {1 => 2, 2 => 2, 5 => 6, 6 => 6, 7 => 8, 8 => 8}[ranking_id]
+    ranking = Ranking.find_by_name(params[:str]) || Ranking.find_by_id(params[:str]) || Ranking.where('kind = "kojin" AND is_registerable IS NULL').last
+    ranking_plus = ranking_minus = nil
+    if ranking.kind == "kojin"
+      ranking_plus = ranking
+      ranking_minus = Ranking.where(["name LIKE ? AND kind = ?", "#{ranking.name[0...4]}%", "kuso"]).last
+    else ranking.kind == "kuso"
+      ranking_puls = Ranking.where(["name LIKE ? AND kind = ?", "#{ranking.name[0...4]}%", "kojin"]).last
+      ranking_minus = ranking
+    end
+    if (!ranking_plus.is_registerable && !ranking_minus.is_registerable) || current_user
       sql = "SELECT s.id, s.name, s.topic_id, s.post_id, " +
         "\"合計得点: \"||rs.mark||\"　糞補正後得点: \"||(rs.mark + COALESCE(rk.mark,0))||\"　重複数: \"||(COALESCE(rs.count,0))||\"　糞重複数: \"||(COALESCE(rk.count, 0))||\"　コメント数: \"||COALESCE(pc.countp, 0)||\"　最高順位: \"||rs.min_rank AS url, " +
         "(rs.mark + COALESCE(rk.mark,0)) AS amark " +
         "FROM series s " +
         "INNER JOIN (" +
-        "SELECT (SUM(31 - rank) + ((COUNT(*) - 1) * 3)) AS mark, serie_id, count(id) AS count, MIN(rank) AS min_rank FROM ranks WHERE ranking_id=#{ranking_id_plus} GROUP BY serie_id" +
+        "SELECT (SUM(#{ranking_plus.scope_max + 1} - rank) + ((COUNT(*) - 1) * 3)) AS mark, serie_id, count(id) AS count, MIN(rank) AS min_rank FROM ranks WHERE ranking_id=#{ranking_plus.id} GROUP BY serie_id" +
         ") rs ON s.id=rs.serie_id " +
         "LEFT JOIN (" +
-        "SELECT (SUM(rank - 6) * 2 + (COUNT(*) -1) * (-3)) AS mark, serie_id, count(id) AS count FROM ranks WHERE ranking_id=#{ranking_id_minus} GROUP BY serie_id" +
+        "SELECT (SUM(rank - #{ranking_minus.scope_max + 1}) * 2 + (COUNT(*) -1) * (-3)) AS mark, serie_id, count(id) AS count FROM ranks WHERE ranking_id=#{ranking_minus.id} GROUP BY serie_id" +
         ") rk ON s.id = rk.serie_id " +
         "LEFT JOIN (" +
-        "SELECT COUNT(p.id) AS countp, p.topic_id FROM posts p WHERE p.created_at > (SELECT created_at FROM rankings WHERE id=#{ranking_id_plus}) GROUP BY p.topic_id" +
+        "SELECT COUNT(p.id) AS countp, p.topic_id FROM posts p WHERE p.created_at > (SELECT created_at FROM rankings WHERE id=#{ranking_plus.id}) GROUP BY p.topic_id" +
         ") pc ON s.topic_id=pc.topic_id"
-      if ranking_id == ranking_id_plus
+      if ranking == ranking_plus
         sql += " order by rs.mark DESC, rs.count DESC, rs.min_rank, rk.count"
-      else ranking_id == ranking_id_minus
+      else ranking == ranking_minus
         sql += " order by amark DESC, rs.count DESC, rs.min_rank, rk.count"
       end
 
-      @ranking_ids = [ranking_id_plus, ranking_id_minus]
+      @ranking_ids = [ranking_plus.id, ranking_minus.id]
       @series = Serie.find_by_sql(sql)
       @series.map! do |serie|
         if /^合計得点\:\s(\d+)　糞補正後得点\:\s(\-?\d+)　重複数\:\s(\d+)　糞重複数\:\s(\d+)　コメント数\:\s(\d+)　最高順位\:\s(\d+)$/ =~ serie.url
@@ -63,7 +67,7 @@ class SeriesController < ApplicationController
         end
         serie
       end
-      if ranking_id == ranking_id_plus
+      if ranking == ranking_plus
         rank = rank_ = sum_of_mark = count_rank = min_rank = 0
         @series.map! do |serie|
           rank_ += 1
@@ -76,7 +80,7 @@ class SeriesController < ApplicationController
           serie.url[:rank] = rank
           serie
         end
-      else ranking_id == ranking_id_minus
+      else ranking == ranking_minus
         rank = rank_ = sum_of_mark_with_kuso = count_kuso = min_rank = 0
         @series.map! do |serie|
           rank_ += 1
@@ -97,7 +101,7 @@ class SeriesController < ApplicationController
         format.txt  { render :txt => @series = @series[0..55] }
       end
     else
-      redirect_to root_path
+      redirect_to root_path, :notice => "ランキングは集計中なのでメンバーだけが見れるよ"
     end
   end
 
