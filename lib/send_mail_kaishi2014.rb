bfd75a99993a29e5bc026e3235328c0ab3ddb694
hidesys <info@hidesys.net>
2014-11-16 07:10:44 +0900

    メール送信先を間違えてた

--- lib/send_mail_kaishi2014.rb
+++ lib/send_mail_kaishi2014.rb
@@ -1,6 +1,7 @@
 def send_mail_kaishi2014_kojin_ranking
-  user = User.find_by_name("黒鷺")
-  ([user.pcmail, user.mbmail] + user.userauths.map(&:email)).uniq.compact.select{|mail| !mail.empty?}.each do |mail|
-    KaishiMailer::kaishi2014_kojin_ranking(user, mail).deliver
+  (User.includes(:ranks).where("ranks.created_at > ?", Time.now - 1.year) + User.where("created_at > ?", Time.now - 6.month)).uniq.each do |user|
+    ([user.pcmail, user.mbmail] + user.userauths.map(&:email)).uniq.compact.select{|mail| !mail.empty?}.each do |mail|
+      KaishiMailer::kaishi2014_kojin_ranking(user, mail).deliver
+    end
   end
 end
