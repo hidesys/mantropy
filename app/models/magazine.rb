451370ef76ebf3d5abd72b8e9a56e36345291296
hidesys <info@hidesys.net>
2017-10-01 13:32:43 +0900

    Serie#Createを直した(Modelでoptional:trueをした)

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -2,5 +2,5 @@ class Magazine < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
   has_many :series, :through => :magazines_series
   has_many :magazines_series
-  belongs_to :book
+  belongs_to :book, optional: true
 end
