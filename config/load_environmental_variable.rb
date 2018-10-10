9ce030253083a330d4ec22491f3e6bb0cacf2168
hidesys <info@hidesys.net>
2018-10-10 17:25:14 +0900

    amazonrcを作成するモンキーパッチ

--- config/load_environmental_variable.rb
+++ config/load_environmental_variable.rb
@@ -6,3 +6,16 @@ DEVISE_MAILER_SECRET = ENV['DEVISE_MAILER_SECRET']
 MAILER_DOMAIN = ENV['MAILER_DOMAIN']
 MAILER_USER = ENV['MAILER_USER']
 MAILER_PASS = ENV['MAILER_PASS']
+
+temp_path = "#{Dir.pwd}/tmp/aaws_cache"
+amazonrc_path = "#{Dir.pwd}/config/amazonrc"
+amazonrc = <<"EOS"
+[global]
+  key_id        = '#{ENV['AMAZONRC_KEY_ID']}'
+  secret_key_id = '#{ENV['AMAZONRC_SECRET']}'
+  locale        = "jp"
+  cache         = false
+  cache_dir     = "#{temp_path}"
+EOS
+File.open(amazonrc_path, "w").write(amazonrc)
+Dir.mkdir(temp_path)
