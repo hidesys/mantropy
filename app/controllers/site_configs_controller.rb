dd1491bb555ebe422810cc1a36c68e74c365790e
hidesys <info@hidesys.net>
2018-10-17 18:56:01 +0900

    Controllerの名前が誤ってた

--- app/controllers/site_configs_controller.rb
+++ app/controllers/site_configs_controller.rb
@@ -1,4 +1,4 @@
-class SiteConfigController < ApplicationController
+class SiteConfigsController < ApplicationController
   def create
     @site_config = SiteConfig.new(site_config_params)
     @site_config.save!
