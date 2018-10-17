3cfe7615a2763475e6b72f444de89a64608a4e75
hidesys <info@hidesys.net>
2018-10-17 19:06:30 +0900

    self.config

--- app/models/site_config.rb
+++ app/models/site_config.rb
@@ -1,6 +1,6 @@
 class SiteConfig < ActiveRecord::Base
-  def self.config(name)
-    site_config = self.find_by_name(name)
+  def self.config(path)
+    site_config = self.find_by_path(path)
     value = site_config && site_config.value
     if value == '' || value == 'false'
       value = false
