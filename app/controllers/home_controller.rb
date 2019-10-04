8be49fa20039a089b5d2e640376b1d07dd8e24c4
Hidesys Megumi <info@hidesys.net>
2019-10-04 19:50:54 +0900

    DBに何もなくてもエラーにしない

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@
 class HomeController < ApplicationController
   def index
     @title = "京大の漫画読みサークル 京大漫トロピー"
-    @content = HikiDoc.to_html(Wiki.where(name: "top").order("created_at DESC").limit(1)[0].content)
+    @content = HikiDoc.to_html(Wiki.where(name: "top").order("created_at DESC").limit(1)[0]&.content || '')
   end
 
   def how_to_use
