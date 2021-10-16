b0cd11622bece5453920d048620d05fd154eb619
Nakashoya <nakashoya@hidetaro.com>
2021-10-17 00:00:25 +0900

    posts

--- app/controllers/member/posts_controller.rb
+++ app/controllers/member/posts_controller.rb
@@ -1,50 +1,26 @@
 # encoding: UTF-8
 class PostsController < Member::Base
-  # GET /posts
-  # GET /posts.xml
   def index
     @posts = Post.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @posts }
-    end
   end
 
-  # GET /posts/1
-  # GET /posts/1.xml
   def show
     @post = Post.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @post }
-    end
   end
 
-  # GET /posts/new
-  # GET /posts/new.xml
   def new
     @post = Post.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @post }
-    end
   end
 
-  # GET /posts/1/edit
   def edit
     @post = Post.find(params[:id])
   end
 
-  # POST /posts
-  # POST /posts.xml
   def create
     @post = Post.new(post_params)
     @post.topic_id = params[:topic_id]
     topic = @post.topic
-    redirect_path = (topic.title ? topic : serie_path(Serie.find_by_topic_id(topic.id)))
+    redirect_path = (topic.title ? member_topic_path(topic) : serie_path(Serie.find_by_topic_id(topic.id)))
     begin
       Post.transaction do
         @post.user = current_user
@@ -62,34 +38,25 @@ class PostsController < Member::Base
       irc_write("[#{(tt = topic.title) ? topic.title : (s = Serie.find_by_topic_id(topic.id)).name}] #{@post.content}", tt ? topic_path(topic) : serie_path(s))
   end
 
-  # PUT /posts/1
-  # PUT /posts/1.xml
   def update
     @post = Post.find(params[:id])
 
-    respond_to do |format|
-      if @post.update_attributes(post_params)
-        format.html { redirect_to(@post, :notice => 'Post was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
-      end
+    if @post.update_attributes(post_params)
+      redirect_to(member_post_path(@post), :notice => 'Post was successfully updated.')
+    else
+      render :action => "edit"
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.xml
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(posts_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(member_posts_path)
   end
+
   private
+
   def post_params
     params.require(:post).permit(
       :name,
