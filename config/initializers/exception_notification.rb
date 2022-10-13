29c99eae51bfa7b00f4bee89c95158e852bc8f1a
ひでシス / Hidesys Megumi <nakashoya@hidetaro.com>
2022-10-13 16:20:20 +0900

    SLACK_WEBHOOK_URLを環境変数に外出しする (#74)
    * SlackTokenを再設定
    
    * SLACK_WEBHOOK_URLを環境変数に外出しする

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,13 +1,11 @@
 require 'exception_notification/rails'
 
-SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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'.freeze
-
 if Rails.env.production?
   ExceptionNotification.configure do |config|
     config.add_notifier :slack, {
       username: 'エラー通知太郎',
       icon_emoji: ':japanese_ogre:',
-      webhook_url: SLACK_WEBHOOK_URL,
+      webhook_url: ENV['SLACK_WEBHOOK_URL'],
       channel: '#4_mantropy_errors'
     }
   end
