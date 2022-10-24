1edea77c6a434f997643ed17ad0b433421ce68ca
Nakashoya Hidesys Megumi <nakashoya@hidetaro.com>
2022-10-24 21:01:44 +0900

    ランキングが存在しません

--- app/controllers/rankings/base.rb
+++ app/controllers/rankings/base.rb
@@ -5,5 +5,7 @@ class Rankings::Base < ApplicationController
 
   def set_ranking
     @ranking = Ranking.find_by(id: params[:ranking_id]) || Ranking.find_by(name: params[:ranking_id])
+    
+    redirect_to(rankings_path, notice: 'ランキングが存在しません') if @ranking.nil?
   end
 end
