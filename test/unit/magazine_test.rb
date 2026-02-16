87f63cff7a1dfebba1be25bf4989d8c55549440b
Hidesys Nakashoya Megumi <nakashoya@hidetaro.com>
2026-02-16 11:51:00 +0900

    del unused tests

--- test/unit/magazine_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class MagazineTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test 'the truth' do
-    assert true
-  end
-end
--- test/unit/magazines_serie_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class MagazinesSerieTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
